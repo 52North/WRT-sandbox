ca884ecab93ab89ef9d1f6c776ca24d089267ce1
jonathanbauer03 <jonathan.bauer03@icloud.com>
2025-04-15 10:50:50 +0200

    add delete_Images_WRT and edit execute-WRT

---
+++ delete_Images_WRT.py
@@ -0,0 +1,10 @@
+import shutil
+
+folder_path = 'Images-WRT'
+
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    if os.path.isfile(file_path) or os.path.islink(file_path):
+        os.remove(file_path)
+    elif os.path.isdir(file_path):
+        shutil.rmtree(file_path)
